36c6c552049f731462ea9930f3e68fc045be6714
NeerajYadav0 <neerajrao04@gmail.com>
2023-07-07 10:13:06 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import Home from './home';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
 
 
 function App() {
-    <Router>
-    <Routes>
-      <Route exact path="/" component={Home} />
-      <Route path="/certificate" component={Home} />
-    </Routes>
-  </Router>
+  return <Home/>
 }
 
 export default App;
