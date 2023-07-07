2fd9022abf1288c4c04634cdfa06f4674c0bfca0
NeerajYadav0 <neerajrao04@gmail.com>
2023-07-07 10:38:38 +0530

    a

--- src/home.jsx
+++ src/home.jsx
@@ -91,111 +91,110 @@ export default function Home() {
       document.head.appendChild(metaTag);
     });
     console.log('data')
-    return null;
+    return  <div className='container'>
+      
+    <Grid container className='d-flex flex-wrap justify-content-evenly'>
+      <Grid item md={8} sm={12} xs={12} className='d-flex flex-column gap-3'>
+        <div >
+          <img style={{ width: '100%' }} src={candidateData.certificateLink} alt='certificate img' />
+        </div>
+        <div>
+          {candidateData.certificateDesc}
+        </div>
+      </Grid>
+      <Grid item md={3} sm={12} xs={12} className='d-flex flex-column gap-3'>
+        <div style={{ fontWeight: 'bold', fontSize: "20px" }}>Certificate Recipient:</div>
+        <div className='d-flex gap-2 align-items-center'>
+          {photo ? (
+            <img
+              src={API_URL.PHOTO_PRE + photo}
+              className="photo-in-upload-profile logo"
+              alt={UI.ALT_CANDIDATE_PROFILE}
+            />
+          ) : (
+            <AccountCircleIcon
+              sx={{
+                borderRadius: '50%',
+                backgroundColor: 'gray',
+                width: '3.25rem',
+                height: '3.25rem',
+                p: 1
+              }}
+            />
+          )}
+          <span className='ml-2'>{candidateData.name}</span>
+        </div>
+        <div style={{ fontWeight: 'bold', fontSize: "20px" }}>
+          About Assessment:
+        </div>
+        <div style={{ fontWeight: 'bold' }}>{candidateData.assessmentName}</div>
+        <div>
+          <span style={{ fontWeight: 'bold' }}>Description: </span><span style={{ color: 'blue' }}>{candidateData.assessmentDesc}</span>
+        </div>
+        <div>
+          <span style={{ fontWeight: 'bold' }}>Rating: </span><span style={{ color: 'red' }}>{candidateData.rating}</span>
+        </div>
+        <div className='d-flex flex-wrap justify-content-between' >
+          <Button
+            onClick={handelDownload}
+            startIcon={<DownloadIcon />}
+          >
+            <span
+              className='color-1D8FF2'
+            >
+              {UI.DOWNLOAD}
+            </span>
+          </Button>
+          <Button
+            onClick={handelShare}
+            startIcon={
+              <ShareIcon />
+            }
+          >
+            <span
+              className='color-1D8FF2'
+
+            >
+              {UI.SHARE}
+            </span>
+            <Menu
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+
+            >
+              <Grid container direction="row" >
+                <Grid item>
+                  <MenuItem>
+                    <Link to=' '> <LinkedIn /></Link>
+                  </MenuItem>
+                </Grid>
+                <Grid item>
+                  <MenuItem >
+                    <Link to=' '> <Facebook /></Link>
+                  </MenuItem>
+                </Grid>
+              </Grid>
+            </Menu>
+          </Button>
+          <Button
+            onClick={handelExplore}
+            startIcon={<UnfoldMoreIcon />}
+          >
+            <span
+              className='color-1D8FF2'
+            >
+              {UI.EXPLORE}
+            </span>
+          </Button>
+        </div>
+
+      </Grid>
+    </Grid>
+  </div>;
   }
   return (<>    
   {render()}
-  <div className='container'>
-      
-      <Grid container className='d-flex flex-wrap justify-content-evenly'>
-        <Grid item md={8} sm={12} xs={12} className='d-flex flex-column gap-3'>
-          <div >
-            <img style={{ width: '100%' }} src={candidateData.certificateLink} alt='certificate img' />
-          </div>
-          <div>
-            {candidateData.certificateDesc}
-          </div>
-        </Grid>
-        <Grid item md={3} sm={12} xs={12} className='d-flex flex-column gap-3'>
-          <div style={{ fontWeight: 'bold', fontSize: "20px" }}>Certificate Recipient:</div>
-          <div className='d-flex gap-2 align-items-center'>
-            {photo ? (
-              <img
-                src={API_URL.PHOTO_PRE + photo}
-                className="photo-in-upload-profile logo"
-                alt={UI.ALT_CANDIDATE_PROFILE}
-              />
-            ) : (
-              <AccountCircleIcon
-                sx={{
-                  borderRadius: '50%',
-                  backgroundColor: 'gray',
-                  width: '3.25rem',
-                  height: '3.25rem',
-                  p: 1
-                }}
-              />
-            )}
-            <span className='ml-2'>{candidateData.name}</span>
-          </div>
-          <div style={{ fontWeight: 'bold', fontSize: "20px" }}>
-            About Assessment:
-          </div>
-          <div style={{ fontWeight: 'bold' }}>{candidateData.assessmentName}</div>
-          <div>
-            <span style={{ fontWeight: 'bold' }}>Description: </span><span style={{ color: 'blue' }}>{candidateData.assessmentDesc}</span>
-          </div>
-          <div>
-            <span style={{ fontWeight: 'bold' }}>Rating: </span><span style={{ color: 'red' }}>{candidateData.rating}</span>
-          </div>
-          <div className='d-flex flex-wrap justify-content-between' >
-            <Button
-              onClick={handelDownload}
-              startIcon={<DownloadIcon />}
-            >
-              <span
-                className='color-1D8FF2'
-              >
-                {UI.DOWNLOAD}
-              </span>
-            </Button>
-            <Button
-              onClick={handelShare}
-              startIcon={
-                <ShareIcon />
-              }
-            >
-              <span
-                className='color-1D8FF2'
-
-              >
-                {UI.SHARE}
-              </span>
-              <Menu
-                anchorEl={anchorEl}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-
-              >
-                <Grid container direction="row" >
-                  <Grid item>
-                    <MenuItem>
-                      <Link to=' '> <LinkedIn /></Link>
-                    </MenuItem>
-                  </Grid>
-                  <Grid item>
-                    <MenuItem >
-                      <Link to=' '> <Facebook /></Link>
-                    </MenuItem>
-                  </Grid>
-                </Grid>
-              </Menu>
-            </Button>
-            <Button
-              onClick={handelExplore}
-              startIcon={<UnfoldMoreIcon />}
-            >
-              <span
-                className='color-1D8FF2'
-              >
-                {UI.EXPLORE}
-              </span>
-            </Button>
-          </div>
-
-        </Grid>
-      </Grid>
-    </div>
     </>
   )
 }
